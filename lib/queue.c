4a1f98b740335e092ba35575c082d607904cb09f
Terence Ng <pheotiman@gmail.com>
2013-11-09 15:51:49 +0800

    add C_queue init & destroy

--- lib/queue.c
+++ lib/queue.c
@@ -5,16 +5,46 @@
 #include<stdlib.h>
 
 struct COMMON_QUEUE *C_init_queue() {
+	struct QUEUE_NODE *head;
+	head = malloc(sizeof(struct QUEUE_NODE));
+	if (head==NULL) {
+		fprintf(stderr, "Insufficient memory!!!");
+		return NULL;
+	}
 	struct COMMON_QUEUE *new_queue;
 	new_queue = malloc(sizeof(struct COMMON_QUEUE));
 	if (new_queue==NULL) {
 		fprintf(stderr, "Insufficient memory!!!");
 		return NULL;
 	}
-	new_queue->next = NULL;
-	new_queue->string = NULL;
+	head->next = NULL;
+	head->string = NULL;
+
+	new_queue->q_node = head;
+	new_queue->top = -1;
+	new_queue->base = -1;
 
 	return new_queue;
+}
+
+void C_destroy_queue(struct COMMON_QUEUE *q) {
+	struct QUEUE_NODE *temp;
+	struct QUEUE_NODE *curr;
+	temp = malloc(sizeof(struct QUEUE_NODE));
+	curr = malloc(sizeof(struct QUEUE_NODE));
+	if (temp==NULL || curr==NULL) {
+		return;
+	}
+	curr = q->q_node;
+	while(curr->next!=NULL) {
+		temp = curr;
+		curr = curr->next;
+		free(temp);
+		temp->next = NULL;
+		temp->string = NULL;
+	}
+	q->top = -1;
+	q->base = -1;
 }
 
 
