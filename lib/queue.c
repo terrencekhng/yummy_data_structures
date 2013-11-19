20b5b5fd715e960091166f9a7bf9a27624001027
Terence Ng <pheotiman@gmail.com>
2013-11-19 16:28:46 +0800

    rewrite linked list queue, fix bug #1

--- lib/queue.c
+++ lib/queue.c
@@ -5,115 +5,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/* linked-list queue operations */
-struct COMMON_QUEUE *C_init_queue() {
-	struct QUEUE_NODE *head;
-	head = malloc(sizeof(struct QUEUE_NODE));
-	if (head==NULL) {
-		fprintf(stderr, "Insufficient memory!!!");
-		return NULL;
-	}
-	struct COMMON_QUEUE *new_queue;
-	new_queue = malloc(sizeof(struct COMMON_QUEUE));
-	if (new_queue==NULL) {
-		fprintf(stderr, "Insufficient memory!!!");
-		return NULL;
-	}
-	head->next = NULL;
-	head->string = NULL;
 
-	new_queue->q_node = head;
-
-	return new_queue;
+void C_init_queue(struct COMMON_QUEUE *new_queue) {
+	new_queue->front = new_queue->rear = malloc(sizeof(struct QUEUE_NODE));
+	if (!new_queue->front) {
+		fprintf(stderr, "Insufficient memory!!!");
+		return;
+	}
+	new_queue->front->next = NULL;
 }
 
 void C_destroy_queue(struct COMMON_QUEUE *q) {
-	struct QUEUE_NODE *temp;
-	struct QUEUE_NODE *curr;
-	temp = malloc(sizeof(struct QUEUE_NODE));
-	curr = malloc(sizeof(struct QUEUE_NODE));
-	if (temp==NULL || curr==NULL) {
-		return;
+	while (q->front) {
+		q->rear = q->front->next;
+		free(q->front);
+		q->front = q->rear;
 	}
-	curr = q->q_node;
-	while(curr->next!=NULL) {
-		temp = curr;
-		curr = curr->next;
-		free(temp);
-		temp->next = NULL;
-		temp->string = NULL;
-	}
-}
-
-int C_get_queue_length(struct COMMON_QUEUE *q) {
-	int count;
-	count = 0;
-
-	struct QUEUE_NODE *temp;
-	temp = malloc(sizeof(struct QUEUE_NODE));
-	if (temp==NULL) {
-		return ERROR;
-	}
-	temp = q->q_node;
-
-	while (temp->next!=NULL) {
-		count += 1;
-		temp = temp->next;
-	}
-
-	return count;
 }
 
 int C_enqueue(struct COMMON_QUEUE *q, char *in) {
-	if (C_get_queue_length(q)>=QUEUE_MAX_SIZE) {
-		fprintf(stderr, "Linked queue is full!!!");
-		return ERROR;
-	}
 	struct QUEUE_NODE *new_node;
-	struct QUEUE_NODE *temp;
 	new_node = malloc(sizeof(struct QUEUE_NODE));
-	temp = malloc(sizeof(struct QUEUE_NODE));
-	if (new_node==NULL || temp==NULL) {
+	if (new_node==NULL) {
+		fprintf(stderr, "Insufficient memory!!!");
 		return ERROR;
 	}
-	new_node->next = NULL;
-	new_node->string = NULL;
 
-	temp = q->q_node;
-
-	while (temp->next!=NULL) {
-		temp = temp->next;
-	}
-	new_node->next = temp->next;
-	temp->next = new_node;
 	new_node->string = in;
+	new_node->next = NULL;
+	q->rear->next = new_node;
+	q->rear = new_node;
 
 	return OK;
 }
 
 char *C_dequeue(struct COMMON_QUEUE *q) {
-	if (C_get_queue_length(q)==0) {
-		fprintf(stderr, "Linked queue is empty!!!");
+	if (q->front==q->rear) {
+		fprintf(stderr, "Linked-list queue is empty!!!");
 		return NULL;
 	}
-	struct QUEUE_NODE *p;
-	p = malloc(sizeof(struct QUEUE_NODE));
-	if (p==NULL) {
-		return NULL;
-	}
-	p->next = NULL;
-	p->string = NULL;
-	p = q->q_node;
+	struct QUEUE_NODE *temp;
+	char *out;
 
-	char *temp;
-	temp = p->next->string;
-	p = p->next;
-	q->q_node->next = p->next;
-	free(p);
-	p->next = NULL;
-	p->string = NULL;
-	
-	return temp;
+	temp = q->front->next;
+	out = temp->string;
+	if (q->rear==temp) {
+		q->rear = q->front;
+	}
+	else {
+		q->front->next = temp->next;
+	}
+	return out;
 }
 
 /* cyclic queue operations */
@@ -142,15 +84,16 @@ void CQ_destroy_queue(struct CYCLIC_QUEUE *q) {
 	int i;
 	
 	for (i = 0; i < q->size; ++i) {
-		free(q->string[i]);
 		q->string[i] = NULL;
+		free(q->string[i]);
+		//q->string[i] = NULL;
 	}
 	q->size = 0;
 	q->top = q->base = 0;
 }
 
 int CQ_is_queue_empty(struct CYCLIC_QUEUE *q) {
-	if (q->size==0) {
+	if (q->top==q->base) {
 		return YES;
 	}
 
@@ -158,25 +101,52 @@ int CQ_is_queue_empty(struct CYCLIC_QUEUE *q) {
 }
 
 int CQ_is_queue_full(struct CYCLIC_QUEUE *q) {
+	if ((q->top+1)%q->size==q->base) {
+		printf("now: %d\n", (q->top + 1)%q->size);
+		return YES;
+	}
 
 	return NO;
 }
 
-void CQ_enqueue(struct CYCLIC_QUEUE *q, char *in) {
+int CQ_get_queue_length(struct CYCLIC_QUEUE *q) {
 	/*if (CQ_is_queue_full(q)==YES) {
+		return q->size;
+	}
+	else if (CQ_is_queue_empty(q)==YES) {
+		return 0;
+	}*/
+
+	int count;
+	count = 0;
+	int pos;
+	pos = q->base;
+
+	while (pos<q->top) {
+		pos  = (pos + 1) % q->size;
+		count += 1;
+	}
+
+	return count;
+}
+
+void CQ_enqueue(struct CYCLIC_QUEUE *q, char *in) {
+	if (CQ_is_queue_full(q)==YES) {
 		fprintf(stderr, "Cyclic queue is full!!!");
 		return;
-	}*/
+	}
+	printf("_%d_", q->top);
 	q->string[q->top] = in;
+	//printf("_%s_", q->string[q->top]);
 	q->top = (q->top + 1) % q->size;
 
 }
 
 char *CQ_dequeue(struct CYCLIC_QUEUE *q) {
-	if (CQ_is_queue_empty(q)==YES) {
+/*	if (CQ_is_queue_empty(q)==YES) {
 		fprintf(stderr, "Cyclic queue is empty!!!");
 		return NULL;
-	}
+	}*/
 	char *temp;
 	temp = q->string[q->base];
 	q->base = (q->base - 1) % q->size;
